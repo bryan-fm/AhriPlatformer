479928474ae5800f0aaa7d8318b5f2069487f2b2
bryan-fm <43351790+bryan-fm@users.noreply.github.com>
2023-12-19 19:18:18 -0300

    corrigindo animação de andar e pulo

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,7 +11,11 @@ public class PlayerMovement : MonoBehaviour
 
     public float moveSpeed = 40f;
 
-    float horizontalMove = 0f;
+    public float jump = 0f;
+
+    private Vector2 playerInput;
+    private bool shouldJump;
+    private bool canJump;
 
     // Start is called before the first frame update
     void Start()
@@ -22,25 +26,67 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // using "GetAxis" allows for many different control schemes to work here
+        // go to Edit -> Project Settings -> Input to see and change them
+        playerInput = new Vector2(Input.GetAxis("Horizontal"), 0);
+ 
+        if(canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            canJump = false;
+            shouldJump = true;
+        }
+
         bool movingRight = Input.GetAxisRaw("Horizontal") == 1;
         bool movingLeft = Input.GetAxisRaw("Horizontal") == -1;
+        bool jumping = Input.GetKeyDown(KeyCode.Space);
 
         if (movingRight) {
-            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"),0) * moveSpeed;
             myAnim.SetFloat("RightSpeed", 1);
             myAnim.SetFloat("LeftSpeed", 0);
         }
 
         if (movingLeft) {
-            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"),0) * moveSpeed;
             myAnim.SetFloat("LeftSpeed", 1);
             myAnim.SetFloat("RightSpeed", 0);
         }
 
-        if (!movingRight && !movingLeft) {
+        if (jumping) {
+            myAnim.SetFloat("LeftSpeed", 0);
+            myAnim.SetFloat("RightSpeed", 0);
+        }
+
+        if (!movingRight && !movingLeft && canJump) {
             myAnim.SetFloat("LeftSpeed", 0);
             myAnim.SetFloat("RightSpeed", 0);
         }
     
+    }
+
+    // apply physics movement based on input values
+    private void FixedUpdate()
+    {
+        // move
+        if(playerInput != Vector2.zero) {
+            transform.Translate(playerInput * Time.fixedDeltaTime * moveSpeed);
+            Debug.Log(Time.fixedDeltaTime);
+            Debug.Log(playerInput);
+        }
+     
+        // jump
+        if(shouldJump) {
+            shouldJump = false;
+            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        // allow jumping again
+        canJump = true;
+    }
+ 
+    private void OnCollisionExit2D(Collision2D col)
+    {
+
     }
 }
